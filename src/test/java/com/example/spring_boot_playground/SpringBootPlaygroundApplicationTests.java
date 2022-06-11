109266c2ce6ec92f9fe8f696c1757f7423f19b21
mckyhrs <af.18.715@gmail.com>
2022-06-11 22:48:20 +0900

    first commit

---
+++ src/test/java/com/example/spring_boot_playground/SpringBootPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
